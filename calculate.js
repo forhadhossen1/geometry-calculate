422891aeca179247d5834f344e47a37765eab1ed
forhadhossen1 <mr.forhad1030@gamil.com>
2023-08-18 19:41:36 +0600

    rectagle shape add and calculate

--- calculate.js
+++ calculate.js
@@ -1,15 +1,37 @@
+// .....addEventListener FileSystem. ... and function system  ...... 
+
 document.getElementById('triangle-btn').addEventListener('click', function(){
     const baseInput = document.getElementById('base-input');
     const inputBaseString = baseInput.value;
     const inputBase = parseFloat(inputBaseString);
+    baseInput.value = '';
     
 
     const heightInput = document.getElementById('height-input');
     const inputHeightString = heightInput.value;
     const inputHeight = parseFloat(inputHeightString);
+    heightInput.value = '';
 
     const area = 0.5 * inputBase * inputHeight;
     
     const triangleAreaResult = document.getElementById('triangle-result');
     triangleAreaResult.innerText = area;
 })
+
+
+function rectangleCalculate(){
+    const weightInput = document.getElementById('weight-input');
+    const inputWeightString = weightInput.value;
+    const inputWeight = parseFloat(inputWeightString);
+    weightInput.value = '';
+
+    const lengthInput =document.getElementById('length-input');
+    const inputLengthString = lengthInput.value;
+    const inputLength = parseFloat(inputLengthString);
+    lengthInput.value = '';
+
+    const area = inputWeight * inputLength;
+
+    const rectangleAreaResult = document.getElementById('rectangle-result');
+    rectangleAreaResult.innerText = area;
+}
